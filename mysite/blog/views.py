90031b1409acd669ba830f59636e31d3af4126c9
Jose Orlando Poz Ajtun <jpoz862@gmail.com>
2019-09-09 12:47:03 -0600

    Agregado de css

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Publicacion
 
-# Create your views here.
+def post_list(request):
+    posts = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def detalle_pub(request,pk):
+    post = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
